3f1724a40710438ec9589070416751d24af1285e
Raditya <arya.raditya26@gmail.com>
2017-02-01 16:18:35 +0700

    adding npm youtube-searchD

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Twitter = require('twitter');
 var config = require('../config.json')
+var search = require('youtube-search');
 
 var client = new Twitter({
   consumer_key: config.consumer_key,
@@ -14,8 +15,18 @@ router.get('/trends', function (req, res, next) {
   var cekTrends = {id: req.query.q}
 
   client.get('trends/place', cekTrends, function(error, tweets, response) {
+    var trend = tweets[0].trends[0].name
     if(!error){
-      res.send(tweets)
+      var opts = {
+        maxResults: 10,
+        key: 'AIzaSyBzoV1Ucl0yz0D8XnB1ej3ABGVD8xeqWas'
+      };
+
+      search(trend, opts, function(err, results) {
+        if(err) return console.log(err);
+
+        res.send(results);
+      });
     }
   })
 

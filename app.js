edde74ffd5f2114ffd73fb1b343f88d58a26486a
timocodex <timothy.og.siahaan@gmail.com>
2017-02-01 16:40:26 +0700

    apply billboard 100 berhasil

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const config = require('./config.js')
+const Twit = require('twit')
+var billboard = require("billboard-top-100").getChart;
 
+const T = new Twit({
+  consumer_key:         config.consumer_key,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret,
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
 var spotifyApi = new SpotifyWebApi({
   clientId : config.id,
   clientSecret : config.secret
@@ -18,6 +27,28 @@ router.get('/music/:song',function(req,res){
       console.error(err);
     });
 
+})
+
+router.get('/trendMusic/:num',function(req,res){
+  billboard('hot-100', function(songs, err){
+      if (err) console.log(err);
+      res.send(songs[(req.params.num)-1].title); //prints song with rank: 4
+  });
+})
+
+router.get('/playtrendmusic/:num',function(req,res){
+
+  billboard('hot-100', function(songs, err){
+      if (err) console.log(err);
+      var song = songs[(req.params.num)-1].title
+
+      spotifyApi.searchTracks(song)
+        .then(function(data) {
+          res.send(data.body);
+        }, function(err) {
+          console.error(err);
+        });
+  })
 })
 
 app.use('/',router)

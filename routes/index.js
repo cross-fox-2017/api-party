f0cd1619b815690d678081c220a0b3e2bac1cddd
Syanmil <syanmil@yahoo.com>
2017-02-01 17:03:29 +0700

    accu and wikipedia?

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+var weather = require("Openweather-Node")
+var config = require('../auth.js')
+
+//set your API key if you have one
+weather.setAPPID(config.data);
+//set the culture
+weather.setCulture("en");
+//set the forecast type
+weather.setForecastType("daily"); //or "" for 3 hours forecast
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  let location = req.query.location
+  weather.now(location,function(err, aData){
+    if(err) res.send(err);
+    res.json(aData.values.weather[0])
+  })
+})
 
 module.exports = router;

f151f3c392b99459307bd0b420fb5917d87468b9
Didit Suryadi <email": null>
2017-02-01 17:22:33 +0700

    finish-for-zomato

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.json');
+var request = require('superagent');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/restaurant', function(req, res, next) {
+  console.log(req.query.q);
+
+  request
+  .get('https://developers.zomato.com/api/v2.1/search?q='+req.query.q+'&count=2')
+  .set('user-key', config.userKey)
+  .set('Accept', 'application/json')
+  .end(function(err, response){
+    // Calling the end function will send the request
+    if(!err){
+      res.json(JSON.parse(response.text))
+    }
+  });
+
+
 });
 
 module.exports = router;
